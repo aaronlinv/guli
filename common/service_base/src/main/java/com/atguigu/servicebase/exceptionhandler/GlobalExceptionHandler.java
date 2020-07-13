933483f2e354ad3ac4a93ecbcf39032905b39b66
Aaron <1546848781@qq.com>
2020-07-13 22:05:15 +0800

    错误日志输出到文件

--- common/service_base/src/main/java/com/atguigu/servicebase/exceptionhandler/GlobalExceptionHandler.java
+++ common/service_base/src/main/java/com/atguigu/servicebase/exceptionhandler/GlobalExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.atguigu.servicebase.exceptionhandler;
 
 import com.atguigu.commonutils.R;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @ControllerAdvice
 @ResponseBody // 为了能够返回数据 
+@Slf4j
 public class GlobalExceptionHandler {
     // 指定了出现了什么异常
     @ExceptionHandler(Exception.class)
@@ -20,11 +22,17 @@ public class GlobalExceptionHandler {
     public R error(ArithmeticException e){
         e.printStackTrace();
         return R.error().message("执行了特定异常处理");
-    }   
-    
-    
+    }
+
+
+    /**
+     * 自定义异常
+     * @param e 
+     * @return
+     */
     @ExceptionHandler(GuliException.class)
     public R error(GuliException e){
+        log.error(e.getMessage());
         e.printStackTrace();
         return R.error().code(e.getCode()).message(e.getMsg());
     }
