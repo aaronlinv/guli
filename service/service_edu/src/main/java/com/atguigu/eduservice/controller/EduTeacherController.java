2f9e158878d912052c0db654d1f8d11830fe066a
Aaron <1546848781@qq.com>
2020-07-13 15:54:29 +0800

    逻辑删除 返回json的全局时间格式

--- service/service_edu/src/main/java/com/atguigu/eduservice/controller/EduTeacherController.java
+++ service/service_edu/src/main/java/com/atguigu/eduservice/controller/EduTeacherController.java
@@ -5,10 +5,7 @@ import com.atguigu.eduservice.entity.EduTeacher;
 import com.atguigu.eduservice.service.EduTeacherService;
 import com.atguigu.eduservice.service.impl.EduTeacherServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -27,11 +24,24 @@ public class EduTeacherController {
     @Autowired
     EduTeacherService teacherService;
 
+    /**查询所有
+     * @return 
+     */
     @GetMapping("findAll")
     public List<EduTeacher> list() {
         List<EduTeacher> list = teacherService.list(null);
         return list;
     }
 
+    /**
+     * 逻辑删除
+     * @param id 
+     * @return
+     */
+    @DeleteMapping("{id}")
+    public boolean removeTeacher(@PathVariable("id") String id) {
+        boolean flag = teacherService.removeById(id);
+        return flag;
+    }
 }
 
