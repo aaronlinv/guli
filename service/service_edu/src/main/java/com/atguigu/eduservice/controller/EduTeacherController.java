694bb62b5623ce4ef6836a8b6cb96415b3939921
Aaron <1546848781@qq.com>
2020-07-13 17:32:31 +0800

    分页查询

--- service/service_edu/src/main/java/com/atguigu/eduservice/controller/EduTeacherController.java
+++ service/service_edu/src/main/java/com/atguigu/eduservice/controller/EduTeacherController.java
@@ -5,6 +5,7 @@ import com.atguigu.commonutils.R;
 import com.atguigu.eduservice.entity.EduTeacher;
 import com.atguigu.eduservice.service.EduTeacherService;
 import com.atguigu.eduservice.service.impl.EduTeacherServiceImpl;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,7 +21,7 @@ import java.util.List;
  * @author testjava
  * @since 2020-07-13
  */
-@Api(description="讲师管理")
+@Api(description = "讲师管理")
 @RestController
 @RequestMapping("/eduservice/teacher")
 public class EduTeacherController {
@@ -28,30 +29,51 @@ public class EduTeacherController {
     @Autowired
     EduTeacherService teacherService;
 
-    /**查询所有
-     * @return 
+    /**
+     * 查询所有
+     *
+     * @return
      */
     @ApiOperation(value = "所有讲师列表")
     @GetMapping("findAll")
     public R list() {
         List<EduTeacher> list = teacherService.list(null);
-        return R.ok().data("items",list);
+        return R.ok().data("items", list);
     }
 
     /**
      * 逻辑删除
-     * @param id 
+     *
+     * @param id
      * @return
      */
     @ApiOperation(value = "根据ID删除讲师")
     @DeleteMapping("{id}")
     public R removeTeacher(@PathVariable("id") String id) {
         boolean flag = teacherService.removeById(id);
-        if(flag){
-            return R.ok(); 
-        }else{
+        if (flag) {
+            return R.ok();
+        } else {
             return R.error();
         }
+    }
+
+    /**
+     * 分页查询
+     * current 当前页
+     * limit 每页显示数目
+     * @return
+     */
+    @GetMapping("pageTeacher/{current}/{limit}")
+    public R pageListTeacher(@PathVariable("current") long current,
+                             @PathVariable("limit") long limit) {
+        Page<EduTeacher> pageTeacher = new Page<>(current, limit);
+        teacherService.page(pageTeacher, null);
+
+        long total = pageTeacher.getTotal();
+        List<EduTeacher> records = pageTeacher.getRecords();
+
+        return R.ok().data("total",total).data("rows",records);
     }
 }
 
