00f9ff626fc3a9dab4fa33812a8e27b21f1c1ec2
Aaron <1546848781@qq.com>
2020-07-13 15:42:24 +0800

    添加启动 查询所有

--- service/service_edu/src/main/java/com/atguigu/eduservice/controller/EduTeacherController.java
+++ service/service_edu/src/main/java/com/atguigu/eduservice/controller/EduTeacherController.java
@@ -1,9 +1,16 @@
 package com.atguigu.eduservice.controller;
 
 
+import com.atguigu.eduservice.entity.EduTeacher;
+import com.atguigu.eduservice.service.EduTeacherService;
+import com.atguigu.eduservice.service.impl.EduTeacherServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * <p>
@@ -14,8 +21,17 @@ import org.springframework.web.bind.annotation.RestController;
  * @since 2020-07-13
  */
 @RestController
-@RequestMapping("/eduservice/edu-teacher")
+@RequestMapping("/eduservice/teacher")
 public class EduTeacherController {
+
+    @Autowired
+    EduTeacherService teacherService;
+
+    @GetMapping("findAll")
+    public List<EduTeacher> list() {
+        List<EduTeacher> list = teacherService.list(null);
+        return list;
+    }
 
 }
 
