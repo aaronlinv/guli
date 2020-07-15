d5de2272b0f7a30d02526c28e25770422b378809
Aaron <1546848781@qq.com>
2020-07-15 17:56:06 +0800

    跨域

--- service/service_oss/src/main/java/com/atguigu/oss/controller/OssController.java
+++ service/service_oss/src/main/java/com/atguigu/oss/controller/OssController.java
@@ -4,6 +4,7 @@ import com.atguigu.commonutils.R;
 import com.atguigu.oss.service.OssService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +12,8 @@ import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequestMapping("/eduoss/fileoss")
+// 跨越
+@CrossOrigin
 public class OssController {
     
     @Autowired
