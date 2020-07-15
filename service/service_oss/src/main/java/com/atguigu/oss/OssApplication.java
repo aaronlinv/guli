207f41eac0aa436abc2599b00797778e5de6102a
Aaron <1546848781@qq.com>
2020-07-15 17:08:39 +0800

    解决 Failed to determine a suitable driver class

--- service/service_oss/src/main/java/com/atguigu/oss/OssApplication.java
+++ service/service_oss/src/main/java/com/atguigu/oss/OssApplication.java
@@ -2,9 +2,12 @@ package com.atguigu.oss;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
+// 解决报错
+//  Failed to determine a suitable driver class
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 
-@SpringBootApplication
 @ComponentScan(basePackages = {"com.atguigu"})
 public class OssApplication {
     public static void main(String[] args) {
