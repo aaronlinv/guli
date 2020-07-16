5e8a10f88497613dcdfd47a9e1cc286b53d86cab
Aaron <1546848781@qq.com>
2020-07-16 10:12:41 +0800

    easyExcel write操作

---
+++ service/service_edu/src/test/java/com.atguigu.demo/excel/TestEasyExcel.java
@@ -0,0 +1,26 @@
+package com.atguigu.demo.excel;
+
+import com.alibaba.excel.EasyExcel;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TestEasyExcel {
+    public static void main(String[] args) {
+        String filename = "D:\\wirte.xlsx";
+        // 路径 实体类
+        EasyExcel.write(filename, DemoData.class).sheet("学生").doWrite(getData());
+
+    }
+
+    private static List<DemoData> getData() {
+        List<DemoData> list = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            DemoData data = new DemoData();
+            data.setSno(i);
+            data.setSname("lucy" + i);
+            list.add(data);
+        }
+        return list;
+    }
+}
