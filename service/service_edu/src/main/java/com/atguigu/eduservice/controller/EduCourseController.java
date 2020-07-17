201e85ba3b64f21ab701494fb080f2ba3b0c2b12
Aaron <1546848781@qq.com>
2020-07-17 17:31:20 +0800

    添加course信息

--- service/service_edu/src/main/java/com/atguigu/eduservice/controller/EduCourseController.java
+++ service/service_edu/src/main/java/com/atguigu/eduservice/controller/EduCourseController.java
@@ -1,6 +1,12 @@
 package com.atguigu.eduservice.controller;
 
 
+import com.atguigu.commonutils.R;
+import com.atguigu.eduservice.entity.vo.CourseInfoVo;
+import com.atguigu.eduservice.service.EduCourseService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.web.bind.annotation.RestController;
@@ -14,8 +20,19 @@ import org.springframework.web.bind.annotation.RestController;
  * @since 2020-07-17
  */
 @RestController
-@RequestMapping("/eduservice/edu-course")
+@RequestMapping("/eduservice/course")
 public class EduCourseController {
+    @Autowired
+    private EduCourseService courseService;
+    
+    // 添加课程信息
+    @PostMapping("addCourseInfo")
+    public R addCourseInfo(@RequestBody CourseInfoVo courseInfoVo){
+        courseService.saveCourseInfo(courseInfoVo);
+        
+        
+        return R.ok();
+    }
 
 }
 
--- service/service_edu/src/main/java/com/atguigu/eduservice/service/EduCourseService.java
+++ service/service_edu/src/main/java/com/atguigu/eduservice/service/EduCourseService.java
@@ -1,6 +1,7 @@
 package com.atguigu.eduservice.service;
 
 import com.atguigu.eduservice.entity.EduCourse;
+import com.atguigu.eduservice.entity.vo.CourseInfoVo;
 import com.baomidou.mybatisplus.extension.service.IService;
 
 /**
@@ -12,5 +13,7 @@ import com.baomidou.mybatisplus.extension.service.IService;
  * @since 2020-07-17
  */
 public interface EduCourseService extends IService<EduCourse> {
-
+    
+    // 添加课程信息
+    void saveCourseInfo(CourseInfoVo courseInfoVo);
 }
