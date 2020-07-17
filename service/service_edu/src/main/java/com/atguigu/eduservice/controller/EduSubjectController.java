b2244606c36f1fcb7517e6ae2a457ac2199eee88
Aaron <1546848781@qq.com>
2020-07-17 15:15:13 +0800

    封装一级分类

--- service/service_edu/src/main/java/com/atguigu/eduservice/controller/EduSubjectController.java
+++ service/service_edu/src/main/java/com/atguigu/eduservice/controller/EduSubjectController.java
@@ -2,11 +2,14 @@ package com.atguigu.eduservice.controller;
 
 
 import com.atguigu.commonutils.R;
+import com.atguigu.eduservice.entity.subject.OneSubject;
 import com.atguigu.eduservice.service.EduSubjectService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
 
 /**
  * <p>
@@ -37,8 +40,8 @@ public class EduSubjectController {
     // 课程分类列表
     @GetMapping("getAllSubject")
     public R getAllSubject(){
-        
-        return R.ok();
+        List<OneSubject> list = subjectService.getAllOneTwoSubject();
+        return R.ok().data("list",list);
     }
             
     
--- service/service_edu/src/main/java/com/atguigu/eduservice/service/impl/EduSubjectServiceImpl.java
+++ service/service_edu/src/main/java/com/atguigu/eduservice/service/impl/EduSubjectServiceImpl.java
@@ -3,14 +3,19 @@ package com.atguigu.eduservice.service.impl;
 import com.alibaba.excel.EasyExcel;
 import com.atguigu.eduservice.entity.EduSubject;
 import com.atguigu.eduservice.entity.excel.SubjectData;
+import com.atguigu.eduservice.entity.subject.OneSubject;
 import com.atguigu.eduservice.listener.SubjectExcelListener;
 import com.atguigu.eduservice.mapper.EduSubjectMapper;
 import com.atguigu.eduservice.service.EduSubjectService;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * <p>
@@ -25,6 +30,7 @@ public class EduSubjectServiceImpl extends ServiceImpl<EduSubjectMapper, EduSubj
 
     /**
      * 添加课程分类
+     *
      * @param file
      * @param subjectService
      */
@@ -36,11 +42,53 @@ public class EduSubjectServiceImpl extends ServiceImpl<EduSubjectMapper, EduSubj
             // 没有传入excel对应实体类报错
             // com.alibaba.excel.exception.ExcelAnalysisException:  
             // java.util.LinkedHashMap cannot be cast to com.al.eduservice.entity.excel.SubjectData
-            EasyExcel.read(in, SubjectData.class,new SubjectExcelListener(subjectService)).sheet().doRead();
-        }catch (Exception e){
-            
+            EasyExcel.read(in, SubjectData.class, new SubjectExcelListener(subjectService)).sheet().doRead();
+        } catch (Exception e) {
+
         }
+
+
+    }
+
+    /**
+     * 获取树形的课程分类列表
+     *
+     * @return
+     */
+    @Override
+    public List<OneSubject> getAllOneTwoSubject() {
+        // 查询一级分类
+        QueryWrapper<EduSubject> wrapperOne = new QueryWrapper<>();
+        wrapperOne.eq("parent_id", "0");
+        List<EduSubject> oneSubjectList = baseMapper.selectList(wrapperOne);
+
+        // 查询二级分类
+        QueryWrapper<EduSubject> wrapperTwo = new QueryWrapper<>();
+        wrapperTwo.ne("parent_id", "0");
+        List<EduSubject> twoSubjectList = baseMapper.selectList(wrapperTwo);
+
+        // 最终集合
+        List<OneSubject> finalSubjectList = new ArrayList<>();
+
+
+        // 封装一级分类
+        for (EduSubject eduSubject : oneSubjectList) {
+            OneSubject oneSubject = new OneSubject();
+            // oneSubject.setId(eduSubject.getId());
+            // oneSubject.setTitle(eduSubject.getTitle());
+
             
+            // Spring包里
+            BeanUtils.copyProperties(eduSubject, oneSubject);
+            
+            
+            finalSubjectList.add(oneSubject);
+        }
+
+
+        // 封装二级分类
         
+
+        return finalSubjectList;
     }
 }
