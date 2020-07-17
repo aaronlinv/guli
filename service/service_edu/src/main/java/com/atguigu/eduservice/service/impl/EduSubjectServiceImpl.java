37334faec5608fb3e8996a67c2d4ad88b7b180cb
Aaron <1546848781@qq.com>
2020-07-17 11:34:33 +0800

    EasyExcel没有传入excel实体类报错笔记

--- service/service_edu/src/main/java/com/atguigu/eduservice/service/impl/EduSubjectServiceImpl.java
+++ service/service_edu/src/main/java/com/atguigu/eduservice/service/impl/EduSubjectServiceImpl.java
@@ -32,6 +32,10 @@ public class EduSubjectServiceImpl extends ServiceImpl<EduSubjectMapper, EduSubj
     public void saveSubject(MultipartFile file, EduSubjectService subjectService) {
         try {
             InputStream in = file.getInputStream();
+            // 注意!! (流，excel对应的实体类，Listener)
+            // 没有传入excel对应实体类报错
+            // com.alibaba.excel.exception.ExcelAnalysisException:  
+            // java.util.LinkedHashMap cannot be cast to com.al.eduservice.entity.excel.SubjectData
             EasyExcel.read(in, SubjectData.class,new SubjectExcelListener(subjectService)).sheet().doRead();
         }catch (Exception e){
             
