a852fa229d33bb7e4f93d254ce439de47c2118f7
Aaron <1546848781@qq.com>
2020-07-18 09:39:44 +0800

    查询章节和小节service实现

--- service/service_edu/src/main/java/com/atguigu/eduservice/service/impl/EduChapterServiceImpl.java
+++ service/service_edu/src/main/java/com/atguigu/eduservice/service/impl/EduChapterServiceImpl.java
@@ -1,12 +1,19 @@
 package com.atguigu.eduservice.service.impl;
 
 import com.atguigu.eduservice.entity.EduChapter;
+import com.atguigu.eduservice.entity.EduVideo;
 import com.atguigu.eduservice.entity.chapter.ChapterVo;
+import com.atguigu.eduservice.entity.chapter.VideoVo;
 import com.atguigu.eduservice.mapper.EduChapterMapper;
 import com.atguigu.eduservice.service.EduChapterService;
+import com.atguigu.eduservice.service.EduVideoService;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -20,10 +27,51 @@ import java.util.List;
 @Service
 public class EduChapterServiceImpl extends ServiceImpl<EduChapterMapper, EduChapter> implements EduChapterService {
 
+    @Autowired
+    private EduVideoService videoService;
+
     @Override
     public List<ChapterVo> getChapterVideoByCourseId(String courseId) {
-        
-        
-        return null;
+        // 1.根据id 查章节
+        QueryWrapper<EduChapter> wrapperChapter = new QueryWrapper<>();
+        wrapperChapter.eq("course_id", courseId);
+
+        List<EduChapter> eduChapterList = baseMapper.selectList(wrapperChapter);
+
+
+        // 2.根据章节 查小节
+        QueryWrapper<EduVideo> wrapperVideo = new QueryWrapper<>();
+        wrapperVideo.eq("course_id", courseId);
+        List<EduVideo> eduVideoList = videoService.list(wrapperVideo);
+
+
+        // 3.遍历章节list 进行封装
+        List<ChapterVo> finalList = new ArrayList<>();
+        for (EduChapter eduChapter : eduChapterList) {
+
+            ChapterVo chapterVo = new ChapterVo();
+            BeanUtils.copyProperties(eduChapter, chapterVo);
+
+            
+            
+            finalList.add(chapterVo);
+            // 4.遍历小节list 进行封装
+            List<VideoVo> videoList = new ArrayList<>();
+            
+            for (EduVideo eduVideo : eduVideoList) {
+                if(eduVideo.getChapterId().equals(eduChapter.getId())){
+                    VideoVo videoVo = new VideoVo();
+                    
+                    BeanUtils.copyProperties(eduVideo, videoVo);
+                    videoList.add(videoVo);
+                }
+                
+            }
+            chapterVo.setChildren(videoList);
+            
+        }
+
+
+        return finalList;
     }
 }
