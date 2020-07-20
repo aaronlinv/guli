d50c7457f2c304a13bf3976a0c18de2da44e14b8
Aaron <1546848781@qq.com>
2020-07-20 11:57:56 +0800

    edu_oss 服务发现：properties和注解

--- service/service_oss/src/main/java/com/atguigu/oss/OssApplication.java
+++ service/service_oss/src/main/java/com/atguigu/oss/OssApplication.java
@@ -3,12 +3,14 @@ package com.atguigu.oss;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 // 解决报错
 //  Failed to determine a suitable driver class
 @SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 
 @ComponentScan(basePackages = {"com.atguigu"})
+@EnableDiscoveryClient
 public class OssApplication {
     public static void main(String[] args) {
         SpringApplication.run(OssApplication.class, args);
