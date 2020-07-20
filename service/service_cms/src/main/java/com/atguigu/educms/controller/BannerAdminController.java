3f286d9200d52ae634183000b1b22d53c5089a71
Aaron <1546848781@qq.com>
2020-07-21 06:27:54 +0800

    Banner后台Controller

--- service/service_cms/src/main/java/com/atguigu/educms/controller/BannerAdminController.java
+++ service/service_cms/src/main/java/com/atguigu/educms/controller/BannerAdminController.java
@@ -1,10 +1,13 @@
 package com.atguigu.educms.controller;
 
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-import org.springframework.web.bind.annotation.RestController;
+import com.atguigu.commonutils.R;
+import com.atguigu.educms.entity.CrmBanner;
+import com.atguigu.educms.service.CrmBannerService;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * <p>
@@ -18,6 +21,57 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/educms/banneradmin")
 @CrossOrigin
 public class BannerAdminController {
+    @Autowired
+    private  CrmBannerService bannerService; 
+    
+    
+    // 分页查询
+    @GetMapping("pageBanner/{page}/{limit}")
+    public R pageBanner(@PathVariable("page") long page,
+                        @PathVariable("limit") long limit){
+        Page<CrmBanner> bannerPage =new Page<>(page,limit);
 
+        bannerService.page(bannerPage, null);
+
+
+        return R.ok().data("items",bannerPage.getRecords()).data("total",bannerPage.getTotal());
+    }
+    
+    // 根据id查
+    @GetMapping("get/{id}")
+    public  R get(@PathVariable("id")String id){
+        CrmBanner banner = bannerService.getById(id);
+        return R.ok().data("item",banner);
+    }
+    
+    // 添加banner
+    @PostMapping("addBanner")
+    public R addBanner(@RequestBody CrmBanner banner){
+        boolean save = bannerService.save(banner);
+        if (save) {
+            return R.ok();
+        }else{
+            return R.error();
+        }
+    }
+    
+    
+    // 修改Banner
+    @PostMapping("update")
+    public R updateBanner(@RequestBody CrmBanner banner){
+        boolean update = bannerService.updateById(banner);
+        if (update) {
+            return R.ok();
+        }else{
+            return R.error();
+        }
+    }
+    
+    // 删除
+    @DeleteMapping("remove/{bannerId}")
+    public R deleteBanner(@PathVariable("bannerId") String bannerId){
+        bannerService.removeById(bannerId);
+        return R.ok();
+    }
 }
 
--- service/service_cms/src/main/java/com/atguigu/educms/entity/CrmBanner.java
+++ service/service_cms/src/main/java/com/atguigu/educms/entity/CrmBanner.java
@@ -1,7 +1,10 @@
 package com.atguigu.educms.entity;
 
+import com.baomidou.mybatisplus.annotation.FieldFill;
 import com.baomidou.mybatisplus.annotation.IdType;
 import java.util.Date;
+
+import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableId;
 import java.io.Serializable;
 import io.swagger.annotations.ApiModel;
@@ -45,9 +48,11 @@ public class CrmBanner implements Serializable {
     @ApiModelProperty(value = "逻辑删除 1（true）已删除， 0（false）未删除")
     private Boolean isDeleted;
 
+    @TableField(fill = FieldFill.INSERT)
     @ApiModelProperty(value = "创建时间")
     private Date gmtCreate;
 
+    @TableField(fill = FieldFill.INSERT_UPDATE)
     @ApiModelProperty(value = "更新时间")
     private Date gmtModified;
 
