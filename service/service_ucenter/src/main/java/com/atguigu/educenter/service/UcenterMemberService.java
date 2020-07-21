8d01c646c0bd226cd7ed0de63bc8001b72a11bd6
Aaron <1546848781@qq.com>
2020-07-21 22:31:01 +0800

    登录

--- service/service_ucenter/src/main/java/com/atguigu/educenter/service/UcenterMemberService.java
+++ service/service_ucenter/src/main/java/com/atguigu/educenter/service/UcenterMemberService.java
@@ -1,6 +1,7 @@
 package com.atguigu.educenter.service;
 
 import com.atguigu.educenter.entity.UcenterMember;
+import com.atguigu.educenter.entity.vo.RegisterVo;
 import com.baomidou.mybatisplus.extension.service.IService;
 
 /**
@@ -15,4 +16,6 @@ public interface UcenterMemberService extends IService<UcenterMember> {
     
     // 登录
     String login(UcenterMember member);
+    // 注册
+    void register(RegisterVo registerVo);
 }
--- service/service_ucenter/src/main/java/com/atguigu/educenter/service/impl/UcenterMemberServiceImpl.java
+++ service/service_ucenter/src/main/java/com/atguigu/educenter/service/impl/UcenterMemberServiceImpl.java
@@ -4,11 +4,14 @@ import com.alibaba.nacos.common.util.Md5Utils;
 import com.atguigu.commonutils.JwtUtils;
 import com.atguigu.commonutils.MD5;
 import com.atguigu.educenter.entity.UcenterMember;
+import com.atguigu.educenter.entity.vo.RegisterVo;
 import com.atguigu.educenter.mapper.UcenterMemberMapper;
 import com.atguigu.educenter.service.UcenterMemberService;
 import com.atguigu.servicebase.exceptionhandler.GuliException;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
 
@@ -22,6 +25,8 @@ import org.springframework.util.StringUtils;
  */
 @Service
 public class UcenterMemberServiceImpl extends ServiceImpl<UcenterMemberMapper, UcenterMember> implements UcenterMemberService {
+    @Autowired
+    private RedisTemplate<String, String> redisTemplate;
 
     // 登录
     @Override
@@ -42,7 +47,7 @@ public class UcenterMemberServiceImpl extends ServiceImpl<UcenterMemberMapper, U
         if (mobileMember == null) {
             throw new GuliException(20001, "登录失败（不存在改手机号）");
         }
-        
+
         // 判断密码
         // 先获取MD5加密后的密码
         if (!MD5.encrypt(password).equals(mobileMember.getPassword())) {
@@ -55,5 +60,45 @@ public class UcenterMemberServiceImpl extends ServiceImpl<UcenterMemberMapper, U
         }
         // 登录成功 返回Token
         return JwtUtils.getJwtToken(mobileMember.getId(), mobileMember.getNickname());
+    }
+
+    // 注册
+    @Override
+    public void register(RegisterVo registerVo) {
+        // 获取注册数据
+        String code = registerVo.getCode();
+        String mobile = registerVo.getMobile();
+        String nickname = registerVo.getNickname();
+        String password = registerVo.getPassword();
+
+        // 非空判断
+        if (StringUtils.isEmpty(code) ||
+                StringUtils.isEmpty(mobile) ||
+                StringUtils.isEmpty(nickname) ||
+                StringUtils.isEmpty(password)) {
+            throw new GuliException(20001, "登录失败（存在空输入）");
+        }
+        // 验证验证码
+        String redisCode = redisTemplate.opsForValue().get(mobile);
+        if (!code.equals(redisCode)) {
+            throw new GuliException(20001, "验证码错误");
+        }
+        // 判断表中是否已存在该手机
+        QueryWrapper<UcenterMember> wrapper = new QueryWrapper<>();
+        wrapper.eq("mobile", mobile);
+        Integer count = baseMapper.selectCount(wrapper);
+        if (count > 0) {
+            throw new GuliException(20001, "已经注册了");
+        }
+        UcenterMember member = new UcenterMember();
+        member.setMobile(mobile);
+        member.setNickname(nickname);
+        member.setPassword(MD5.encrypt(password));
+        
+        // 是否禁用
+        member.setIsDisabled(false);
+        
+        member.setAvatar("http://thirdwx.qlogo.cn/mmopen/vi_32/DYAIOgq83eoj0hHXhgJNOTSOFsS4uZs8x1ConecaVOB8eIl115xmJZcT4oCicvia7wMEufibKtTLqiaJeanU2Lpg3w/132");
+        baseMapper.insert(member);
     }
 }
