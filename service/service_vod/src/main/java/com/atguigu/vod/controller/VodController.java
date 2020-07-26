ae12cf91f9ce7ff05f2dfdf1c20e82b411a05792
Aaron <1546848781@qq.com>
2020-07-26 16:20:29 +0800

    根据视频id获取视频播放凭证

--- service/service_vod/src/main/java/com/atguigu/vod/controller/VodController.java
+++ service/service_vod/src/main/java/com/atguigu/vod/controller/VodController.java
@@ -2,6 +2,8 @@ package com.atguigu.vod.controller;
 
 import com.aliyuncs.DefaultAcsClient;
 import com.aliyuncs.vod.model.v20170321.DeleteVideoRequest;
+import com.aliyuncs.vod.model.v20170321.GetVideoPlayAuthRequest;
+import com.aliyuncs.vod.model.v20170321.GetVideoPlayAuthResponse;
 import com.atguigu.commonutils.R;
 import com.atguigu.servicebase.exceptionhandler.GuliException;
 import com.atguigu.vod.service.VodService;
@@ -10,6 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.ws.rs.GET;
 import java.util.List;
 
 @RestController
@@ -44,15 +47,40 @@ public class VodController {
 
         } catch (Exception e) {
             e.printStackTrace();
-            throw  new GuliException(20001, "删除视频失败"); 
+            throw new GuliException(20001, "删除视频失败");
         }
     }
+
     // 删除多个阿里视频
     @DeleteMapping("delete-batch")
     // 传入多个id
-    public R deleteBatch(@RequestParam("videoIdList") List<String> videoIdList){
+    public R deleteBatch(@RequestParam("videoIdList") List<String> videoIdList) {
         vodService.removeMoreAlyVideo(videoIdList);
-        
+
         return R.ok();
     }
+
+    // 根据视频id获取视频播放凭证
+    @GetMapping("getPlayAuth/{id}")
+    public R getPlayAuth(@PathVariable("id") String id) {
+        try {
+            DefaultAcsClient client = InitVodClient.initVodClient();
+            GetVideoPlayAuthRequest request = new GetVideoPlayAuthRequest();
+
+            // 向request设置id
+            request.setVideoId(id);
+
+            // 调用方法得到凭证
+            GetVideoPlayAuthResponse response = client.getAcsResponse(request);
+            String playAuth = response.getPlayAuth();
+            return R.ok().data("playAuth",playAuth);
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new GuliException(20001, "获取凭证失败");
+
+        }
+
+
+    }
+
 }
