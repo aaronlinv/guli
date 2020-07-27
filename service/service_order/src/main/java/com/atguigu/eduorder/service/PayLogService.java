5aeef5a33afa7e5548c7ea128e151414db658792
Aaron <1546848781@qq.com>
2020-07-27 10:42:16 +0800

    生成代码

---
+++ service/service_order/src/main/java/com/atguigu/eduorder/service/PayLogService.java
@@ -0,0 +1,16 @@
+package com.atguigu.eduorder.service;
+
+import com.atguigu.eduorder.entity.PayLog;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * <p>
+ * 支付日志表 服务类
+ * </p>
+ *
+ * @author testjava
+ * @since 2020-07-27
+ */
+public interface PayLogService extends IService<PayLog> {
+
+}
